06234b23fea0ffbaa00f22573f1cc0eb71657ba5
bobohdana <stetsenkobohdana@gmail.com>
2023-05-19 16:51:57 +0300

    added header

---
+++ script.js
@@ -0,0 +1,11 @@
+// header
+const headerBottom = document.querySelector('.bottom')
+
+for (let i = 0; i < 3; i++) {
+  const p = document.createElement('p')
+  p.innerHTML = 'Lorem Ipsum rect aping do'
+  p.classList.add('featureBottom')
+  headerBottom.appendChild(p)
+}
+
+
