42cbf733aaa511ff9bdaf385b60795e26bcbbfc2
bobohdana <stetsenkobohdana@gmail.com>
2023-05-20 15:08:45 +0300

    added the part with a slider

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const productsData = [
     img: 'img/product1.png'
   },
   {
-    id: 2,
+    id: 1,
     title: 'Produkt ABC',
     description: 'Kurzbeschreibung zum Produkt',
     price: '39,00',
@@ -28,7 +28,7 @@ const productsData = [
     img: 'img/product1.png'
   },
   {
-    id: 3,
+    id: 2,
     title: 'Produkt ABC',
     description: 'Kurzbeschreibung zum Produkt',
     price: '39,00',
@@ -36,7 +36,7 @@ const productsData = [
     img: 'img/product2.png'
   },
   {
-    id: 4,
+    id: 3,
     title: 'Produkt ABC',
     description: 'Kurzbeschreibung zum Produkt',
     price: '39,00',
@@ -60,15 +60,19 @@ productsData.forEach((product) => {
   const title = document.createElement('div')
   title.innerHTML = product.title
   title.classList.add('productTitle')
+
   const description = document.createElement('div')
   description.innerHTML = product.description
   description.classList.add('productDesc')
+
   const price = document.createElement('div')
   price.innerHTML = `${product.price} €`
   price.classList.add('productPrice')
+
   const note = document.createElement('div')
   note.innerHTML = product.note
   note.classList.add('productNote')
+
   productContainer.append(title, description, price, note)
 
   const actions = document.createElement('div')
@@ -77,9 +81,119 @@ productsData.forEach((product) => {
   button.innerHTML = 'Zum Produkt'
   button.classList.add('productBtn')
   actions.appendChild(button)
-  productContainer.appendChild(actions)
 
+  productContainer.appendChild(actions)
   productsRoot.appendChild(productContainer)
+})
+
+// avatars
+const avatars = [
+  'img/avatar1.png',
+  'img/avatar2.png',
+  'img/avatar3.png',
+  'img/avatar4.png',
+  'img/avatar5.png',
+  'img/avatar6.png',
+  'img/avatar7.png',
+  'img/avatar8.png',
+]
+const avatarsContainer = document.querySelector('.avatars')
+
+avatars.forEach(avatar => {
+  const div = document.createElement('div')
+  div.classList.add('avatar')
+  const img = document.createElement('img')
+  img.src = avatar
+  img.alt = ''
+  div.appendChild(img)
+  avatarsContainer.appendChild(div)
+})
+
+// reviews
+const reviews = [
+  {
+    id: 0,
+    name: 'Unge Fuchs',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd, no sea takimata sanctus est.0',
+    estimate: 5,
+  },
+  {
+    id: 1,
+    name: 'Tina Labermann',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd, no sea takimata sanctus est.0',
+    estimate: 5,
+  },
+  {
+    id: 2,
+    name: 'Alfred Langhaar',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd, no sea takimata sanctus est.0',
+    estimate: 5,
+  },
+  {
+    id: 3,
+    name: 'Simon Finger',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd, no sea takimata sanctus est.0',
+    estimate: 5,
+  },
+  {
+    id: 4,
+    name: 'Max Schornstein',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd, no sea takimata sanctus est.0',
+    estimate: 5,
+  },
+]
+const reviewsContainer = document.querySelector('.reviews')
+const arrowLeft = document.querySelector('.arrowLeft')
+const arrowRight = document.querySelector('.arrowRight')
+
+reviews.forEach(review => {
+  const reviewDiv = document.createElement('div')
+  reviewDiv.classList.add('review')
+
+  const stars = document.createElement('div')
+  stars.classList.add('estimateRoot')
+  for (let i = 0; i < review.estimate; i++) {
+    const star = document.createElement('img')
+    star.classList.add('.estimate')
+    star.src = 'img/star.png'
+    star.alt = ''
+    stars.appendChild(star)
+  }
+
+  const name = document.createElement('h5')
+  name.classList.add('reviewName')
+  name.innerHTML = review.name
+
+  const text = document.createElement('p')
+  text.classList.add('reviewText')
+  text.innerHTML = review.text
+
+  reviewDiv.append(stars, name, text)
+  reviewsContainer.appendChild(reviewDiv)
+})
+
+const reviewWidth = document.querySelector('.review').offsetWidth
+const margin = 20
+let currentLeft = document.documentElement.clientWidth * 0.5
+
+arrowLeft.addEventListener('click', () => {
+  arrowLeft.classList.remove('hidden')
+  arrowRight.classList.remove('hidden')
+  currentLeft = currentLeft + reviewWidth + margin
+  reviewsContainer.style.left = currentLeft + 'px'
+  if (document.documentElement.clientWidth - (reviewWidth + margin) < currentLeft) {
+    arrowLeft.classList.add('hidden')
+  }
+})
+
+arrowRight.addEventListener('click', () => {
+  arrowRight.classList.remove('hidden')
+  arrowLeft.classList.remove('hidden')
+  currentLeft = currentLeft - reviewWidth - margin
+  reviewsContainer.style.left = currentLeft + 'px'
+  if (currentLeft / (reviewWidth + margin) < 1) {
+    arrowRight.classList.add('hidden')
+  }
 })
 
 
